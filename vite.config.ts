6b78a02d16d3a292269ef713e5593a16373f06a1
tobdseyltd <mail@tobsey.limited>
2024-04-13 08:20:48 +0200

    v.106 - Sitemap Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { sitemapPlugin } from 'sveltekit-sitemap';
 
-/** @type {import('vite').UserConfig} */
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		sitemapPlugin({ routesDir: './src/routes', sitemapFile: './src/sitemap.ts' })
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
